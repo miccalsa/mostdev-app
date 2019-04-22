aa16fc3564b54ccc24c18e1b02349e4b7f9f015d
Mike Calderon <miccalsa@gmail.com>
2019-04-22 17:20:04 +0200

    Initial layout for login page

---
+++ src/pages/public/PreAuth/components/SSO/sso.js
@@ -0,0 +1,24 @@
+import React from 'react';
+
+import './sso.css';
+import yacht_logo from '../../../../../images/logo.png';
+import google_logo from '../../../../../images/g-logo.png';
+
+function SSO() {
+
+    return (
+        <div className="sso_container-login">
+            <img src={yacht_logo} className="sso_container-img" alt="Yacht logo" />
+            <p className="sso_container-description">Login to Your Account</p>
+            <img src={google_logo} className="sso_container-logo" alt="Yacht logo" />
+            <button type="button" className="btn btn-primary sso_container_login-btn">
+                Login
+            </button>
+            <p className="sso_container_small-text">
+                Use your Yacht Google email to login
+            </p>
+        </div>
+    );
+}
+
+export default SSO;
---
+++ src/pages/public/PreAuth/login.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+import LoginLayout from './components/Login/login-layout';
+
+class Login extends Component {
+    render() {
+        return (
+            <LoginLayout />
+        );
+    }
+}
+
+export default Login;
